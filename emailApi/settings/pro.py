76f1fce9f7a264a6945a23f42eafd053273240ee
maricm123 <mihailomaric001@gmail.com>
2022-03-10 12:54:09 +0100

    host credentials

--- emailApi/settings/pro.py
+++ emailApi/settings/pro.py
@@ -24,3 +24,11 @@ REST_FRAMEWORK = {
    ),
    
 }
+
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mihailomaric001@gmail.com'
+EMAIL_HOST_PASSWORD = 'MARIC001'
+EMAIL_USE_TLS = True

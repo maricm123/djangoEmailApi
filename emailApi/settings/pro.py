cd692b74420c31f6798be041602fb07645af1445
maricm123 <mihailomaric001@gmail.com>
2022-03-10 13:56:41 +0100

    host settings

--- emailApi/settings/pro.py
+++ emailApi/settings/pro.py
@@ -30,5 +30,5 @@ REST_FRAMEWORK = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mihailomaric001@gmail.com'
-EMAIL_HOST_PASSWORD = 'MARIC001'
+EMAIL_HOST_PASSWORD = 'fgvflyjyvjzyidse'
 EMAIL_USE_TLS = True

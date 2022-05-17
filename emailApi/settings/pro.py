843cb0e7eec549de3f6a92a9ac29be93ecbc63a9
maricm123 <mihailomaric001@gmail.com>
2022-05-17 21:36:54 +0200

    new settings

--- emailApi/settings/pro.py
+++ emailApi/settings/pro.py
@@ -9,6 +9,8 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     "https://sanjamuskistudio.web.app",
     "https://sanjammuskistudio.rs",
+    "http://localhost:8080"
+
 ]
 
 DATABASES = {
@@ -33,5 +35,6 @@ REST_FRAMEWORK = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sanjamuskistudio@gmail.com'
-EMAIL_HOST_PASSWORD = 'mseghbfkcnatsctr'
+EMAIL_HOST_PASSWORD = 'toqhorrzaiimcnff'
+# EMAIL_HOST_PASSWORD = 'MARIC001'
 EMAIL_USE_TLS = True
--- sendemail/views.py
+++ sendemail/views.py
@@ -13,11 +13,15 @@ def api(request):
      minut=request.GET.get('minut')
      frizer=request.GET.get('frizer')
      datum=request.GET.get('datum')
-     
 
      subject=" Informacije o zakazanom terminu | SanjaM Studio "# The theme
      from_email='sanjamuskistudio@gmail.com'# Give a person
      to_email=receiver# The recipient
+     print(to_email)
      message="Dobar dan {0}".format(name) + ". \n\n" + "Datum vaseg sisanja je {0}".format(datum) + " u {0}".format(sat) + ":{0}".format(minut) + ". Vas frizer je {0}".format(frizer) + ". \n\n" + "Za otkazivanje termina molimo Vas pozovite  +381691303083."
-     send_mail(subject,message,from_email,[to_email])# call mail Of send_mail Function to send mail
+     print(message)
+     try:
+          send_mail(subject,message,from_email,[to_email])# call mail Of send_mail Function to send mail
+     except Exception as e:
+          print(e)
      return HttpResponse(' The mail was sent successfully ')
